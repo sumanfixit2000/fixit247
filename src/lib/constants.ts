778bbdea63eb9e15c9e0b1526f9e5f840d590093
sumanfixit2000 <sumanfixit@gmail.com>
2026-03-06 12:13:24 +0530

    update email

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,7 +7,7 @@ export const CALL_LINK = `tel:${PHONE_NUMBER}`;
 export const INSTAGRAM_LINK =
   "https://www.instagram.com/fixit.24.7?utm_source=qr&igsh=MXh6OGZkamhkdWo1Mg==";
 export const FACEBOOK_LINK = "https://www.facebook.com/share/1Bvc5vwrtz/";
-export const EMAIL = "contact@fixit247.com";
+export const EMAIL = "contact@fixit247.in";
 
 export const MAP =
   "https://www.google.com/maps?q=19.332213,84.768697&z=15&output=embed";
